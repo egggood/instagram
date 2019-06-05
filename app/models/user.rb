8c0da1baef32963a2c8a5efa2cd90e3ba34b444c
ミハエル <mihaeru@mihaerunoMacBook.local>
2019-06-05 18:53:13 +0900

    amended violations of rails style guide in app/models

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,32 +4,31 @@ class User < ApplicationRecord
                                   foreign_key: "follower_id",
                                   dependent:   :destroy
   has_many :passive_relationships, class_name:  "Relationship",
-                                  foreign_key: "followed_id",
-                                  dependent:   :destroy
+                                   foreign_key: "followed_id",
+                                   dependent:   :destroy
   has_many :following, through: :active_relationships,  source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
-  has_many :active_likes, class_name: "Like",foreign_key: "user_id", dependent: :destroy
+  has_many :active_likes, class_name: "Like", foreign_key: "user_id", dependent: :destroy
   has_many :liking, through: :active_likes, source: :micropost
   mount_uploader :profile_picture, PictureUploader
   attr_accessor :remember_token
-  validates :name, presence: true, length:{maximum: 50}
-  validates :user_name, presence: true, length:{maximum: 50}, uniqueness: true
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :user_name, presence: true, length: { maximum: 50 }, uniqueness: true
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  #self.email.empty?だとundifined method for NilClassになった。nil,empty,blankの違いを知らい
-  before_save {self.email = self.email.downcase unless self.email.nil?}
+  # self.email.empty?だとundifined method for NilClassになった。nil,empty,blankの違いを知らい
+  before_save { self.email = self.email.downcase unless self.email.nil? }
   validates :email, presence: true, allow_blank: true,
                     uniqueness: { case_sensitive: false },
                     format: { with: VALID_EMAIL_REGEX }
-  #正規表現でphonenumberを帰省したがその書き方が分からないので後回し
+  # 正規表現でphonenumberを帰省したがその書き方が分からないので後回し
   validates :phonenumber, presence: true, allow_blank: true, uniqueness: true
-  #genderはmaleとfemaleだけしかpassしないようにする
+  # genderはmaleとfemaleだけしかpassしないようにする
   validates :gender, presence: true, allow_blank: true
-  validates :self_introduction, presence: true, allow_blank: true, length:{maximum: 140}
+  validates :self_introduction, presence: true, allow_blank: true, length: { maximum: 140 }
   has_secure_password
-  validates :password, presence: true, length:{minimum: 6}, allow_nil: true
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
   def User.digest(string)
-    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-                                                  BCrypt::Engine.cost
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
   end
 
@@ -47,11 +46,11 @@ class User < ApplicationRecord
   end
 
   def authenticated?(remember_token)
-   return false if remember_digest.nil?
-   BCrypt::Password.new(remember_digest).is_password?(remember_token)
- end
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
 
- # ユーザーをフォローする
+  # ユーザーをフォローする
   def follow(other_user)
     following << other_user
   end
@@ -66,19 +65,18 @@ class User < ApplicationRecord
     following.include?(other_user)
   end
 
-  #投稿にいいねする
-   def like(micropost)
-     liking << micropost
-   end
+  # 投稿にいいねする
+  def like(micropost)
+    liking << micropost
+  end
 
-   #投稿のいいねを取り消す
-   def undo_like(micropost)
-     active_likes.find_by(micropost_id: micropost.id).destroy
-   end
+  # 投稿のいいねを取り消す
+  def undo_like(micropost)
+    active_likes.find_by(micropost_id: micropost.id).destroy
+  end
 
-   # いいねした東欧ならtureを返す
-   def like?(micropost)
-     liking.include?(micropost)
-   end
-
+  # いいねした東欧ならtureを返す
+  def like?(micropost)
+    liking.include?(micropost)
+  end
 end
