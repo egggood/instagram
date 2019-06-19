9733e72bea9e84e063e0a8b699c899246e2aadd1
ミハエル <mihaeru@mihaerunoMacBook.local>
2019-06-19 14:03:36 +0900

    attach if logged_in? to sessions#new

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,16 @@
 class SessionsController < ApplicationController
   def new
+    if logged_in?
+      redirect_to user_path(session[:user_id])
+    end
   end
 
   def create
     user = User.find_by(user_name: params[:session][:user_name])
     if user && user.authenticate(params[:session][:password])
-      flash[:success] = "ログインに成功しました^ ^"
       log_in user
       remember user if params[:session][:remember_me] == '1'
+      flash[:success] = "ログインに成功しました^ ^"
       redirect_to user
     else
       flash[:danger] = "Invalid user_name/password conbination"
