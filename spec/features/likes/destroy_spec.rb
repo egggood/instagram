d4d90485636ae8967ebd2d138e82029340016791
ミハエル <mihaeru@mihaerunoMacBook.local>
2019-06-27 20:09:33 +0900

    refacter spec/features/likes/destroy_spec.rb to remove extra test

--- spec/features/likes/destroy_spec.rb
+++ spec/features/likes/destroy_spec.rb
@@ -2,34 +2,35 @@ require 'rails_helper'
 require 'support/login_support'
 
 RSpec.feature "Likes#destroy", type: :feature do
-  let(:like) { create(:like) }
-  let(:micropost) { create(:micropost, like: like) }
-
   context "ログインしていない時" do
+    let(:user) { create(:user) }
+    let(:micropost) { create(:micropost) }
+    let(:like) { create(:like, micropost: micropost, user: user) }
+
     scenario "login_pathにリダイレクトする" do
-      page.driver.submit(:delete, like_path(micropost.like.like_id), {})
+      page.driver.submit(:delete, like_path(like.id), {})
       expect(page).to have_current_path login_path
     end
   end
 
   context "ログインしている時" do
     let(:user) { create(:user) }
-    let(:like) { create(:like) }
-    let(:micropost) { create(:micropost, user: user, like: like) }
+    let(:micropost) { create(:micropost, user: user) }
 
     before do
+      user.liking << [micropost]
       sign_in user
     end
 
-    scenario "いいねに成功する" do
+    scenario "いいねを取り消す" do
       visit micropost_path micropost.id
       expect(page).to have_current_path micropost_path micropost.id
+      expect(user.like?(micropost)).to be true
       expect(page).not_to have_button "いいね！"
       expect(page).to have_button "いいねを取り消す"
 
       click_button "いいねを取り消す"
       expect(page).to have_current_path micropost_path micropost.id
-      expect(page).to have_content "いいねしました！"
       expect(page).to have_button "いいね！"
       expect(page).not_to have_button "いいねを取り消す"
     end
