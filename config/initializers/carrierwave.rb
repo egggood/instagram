b2824b5bb1503467520968181581adb36fb86cda
ミハエル <mihaeru@mihaerunoMacBook.local>
2019-06-30 21:26:49 +0900

    set aws_access_key_id

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,12 +2,12 @@ if Rails.env.production?
   CarrierWave.configure do |config|
     config.fog_credentials = {
       provider: 'AWS',
-      aws_access_key_id: ENV['ACCESS_KEY_ID'],
-      aws_secret_access_key: ENV['SECRET_ACCESS_KEY'],
+      aws_access_key_id: ENV['S3_ACCESS_KEY'],
+      aws_secret_access_key: ENV['S3_SECRET_KEY'],
       region: 'ap-northeast-1'
     }
 
-    config.fog_directory  = 'kai-instagram-123'
+    config.fog_directory = 'kai-instagram-123'
     config.cache_storage = :fog
   end
 end
